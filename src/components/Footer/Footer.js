4d2bcbc2b963561763c6952a370bf6daa2d38802
Polina Lashchanka <lisaya666@gmail.com>
2023-08-25 21:26:03 +0200

    feat: add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,27 @@
+import "./Footer.css";
+
 function Footer() {
-    return (
-        <h1>text</h1>
-    );
+  return (
+    <footer className="footer">
+      <h2 className="footer__header">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </h2>
+      <div className="footer__info">
+        <p className="footer__date">© 2020</p>
+        <div className="footer__yandex">
+          <p className="footer__text">Яндекс.Практикум</p>
+          <a
+            className="footer__github"
+            href="https://github.com/Yandex-Practicum"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
